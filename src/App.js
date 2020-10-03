442e30945a0fc76109d081d3476b39ef6beae8e5
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-10-03 08:47:56 +0300

    new hw

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    );
+  }
+}
