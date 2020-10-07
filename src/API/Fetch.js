2809324a18199e5b5c7b65cb558229f53c4a0711
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-10-07 20:04:34 +0300

    some fix

--- src/API/Fetch.js
+++ src/API/Fetch.js
@@ -1,6 +1,6 @@
 const fetchSearchPictures = (searchQuery, page) => {
   return fetch(
-    `https://pixabay.com/api/?key=17911527-5bdfde13a28c678a168d7ccf7&q=${searchQuery}&per_page=15&page=${page}`
+    `https://pixabay.com/api/?key=17911527-5bdfde13a28c678a168d7ccf7&q=${searchQuery}&per_page=12&page=${page}`
   ).then((response) => response.json().then((data) => data.hits));
 };
 
